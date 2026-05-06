7c95ce402101552226113174eb706ef1f4638fc1
MrSeIFyR <parsadanovvv@gmail.com>
2026-05-06 18:58:21 +0300

    Update main.py
    Implementing a loop and exiting it when entering the number 5

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ print("2. View all expenses")
 print("3. Calculate total and average expense")
 print("4. Clear all expenses")
 print("5. Exit")
+
+while True:
+    choice = int(input("Введите цифру из меню выше: "))
+    if choice == 5:
+        print("Exiting the Daily Expense Tracker. Goodbye!")
+        break
